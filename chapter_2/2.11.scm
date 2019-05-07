f7fb258a261821ff30a28a4b150f07d09200e96e
ceoro9 <ceoro9@protonmail.com>
2019-05-07 21:13:52 +0300

    Complete all conditions in 2.11 task

--- chapter_2/2.11.scm
+++ chapter_2/2.11.scm
@@ -11,27 +11,87 @@
         (make-interval (min p1 p2 p3 p4) 
                        (max p1 p2 p3 p4))))
 
+(define (is-negative-interval? x)
+    (AND (< (lower-bound x) 0) (< (upper-bound x) 0)))
+
+(define (is-positive-interval? x)
+    (AND (> (lower-bound x) 0) (> (upper-bound x) 0)))
+
+(define (is-crossed-interval? x)
+    (AND (<= (lower-bound x) 0) (>= (upper-bound x) 0)))
+
 (define (new-mul-interval x y)
-    (cond ((And (> (lower-bound x) 0)  
-                (> (lower-bound y) 0))  
-                    (make-interval (* (lower-bound x) (lower-bound y)) 
-                                   (* (upper-bound x) (upper-bound y))))
-          ((And (< (upper-bound x) 0) 
-                (< (upper-bound y) 0))
-                    (make-interval (* (lower-bound x) (lower-bound y)) 
-                                   (* (upper-bound x) (upper-bound y))))
-          ((And (< (lower-bound x) 0)
-                (> (lower-bound y) 0))  
-                    (make-interval (* (lower-bound x) (upper-bound y))
-                                   (* (lower-bound y) (upper-bound x))))
-          ((And (> (lower-bound x) 0)
-                (< (lower-bound y) 0))
-                    (make-interval (* (lower-bound y) (upper-bound x))
-                                   (* (upper-bound x) (upper-bound y))))
-          (()) ; ... TODO:
-          ))
+    (cond ((AND (is-positive-interval? x)
+                (is-positive-interval? y))
+          (make-interval (* (lower-bound x) (lower-bound y)) 
+                         (* (upper-bound x) (upper-bound y))))
+          
+          ((AND (is-negative-interval? x)
+                (is-negative-interval? y))
+          (make-interval (* (upper-bound x) (upper-bound y))
+                         (* (lower-bound x) (lower-bound y))))
 
+          ((AND (is-negative-interval? x)
+                (is-positive-interval? y))
+          (make-interval (* (lower-bound x) (upper-bound y))
+                         (* (upper-bound x) (lower-bound y))))
 
-(display (mul-interval (make-interval 3 5) (make-interval -5 6)))
+          ((AND (is-positive-interval? x)
+                (is-negative-interval? y))
+          (make-interval (* (lower-bound y) (upper-bound x))
+                         (* (upper-bound y) (lower-bound x))))
+
+          ((AND (is-positive-interval? x) 
+                (is-crossed-interval? y))
+          (make-interval (* (upper-bound x) (lower-bound y))
+                         (* (upper-bound x) (upper-bound y))))
+
+          ((AND (is-negative-interval? x) 
+                (is-crossed-interval? y))
+          (make-interval (* (lower-bound x) (upper-bound y))
+                         (* (lower-bound x) (lower-bound y))))
+
+          ((AND (is-positive-interval? y) 
+                (is-crossed-interval? x))
+          (make-interval (* (upper-bound y) (lower-bound x))
+                         (* (upper-bound y) (upper-bound x))))
+
+          ((AND (is-negative-interval? y) 
+                (is-crossed-interval? x))
+          (make-interval (* (lower-bound y) (upper-bound x))
+                         (* (lower-bound y) (lower-bound x))))
+          
+          ((AND (is-crossed-interval? x) 
+                (is-crossed-interval? y))
+          (make-interval (min (* (lower-bound x) (upper-bound y))
+                              (* (upper-bound x) (lower-bound y)))
+                         (max (* (lower-bound x) (lower-bound y))
+                              (* (upper-bound x) (upper-bound y)))))))
+
+(display (mul-interval (make-interval -3 -5) (make-interval -5 -2)))
 (newline)
-(display (new-mul-interval (make-interval 3 5) (make-interval -5 6)))
+(display (new-mul-interval (make-interval -3 -5) (make-interval -5 -2)))
+
+(newline)
+
+(display (mul-interval (make-interval -3 10) (make-interval -5 15)))
+(newline)
+(display (new-mul-interval (make-interval -3 10) (make-interval -5 15)))
+
+(newline)
+
+(display (mul-interval (make-interval 3 5) (make-interval 4 6)))
+(newline)
+(display (new-mul-interval (make-interval 3 5) (make-interval 4 6)))
+
+(newline)
+
+(display (mul-interval (make-interval -3 5) (make-interval 4 6)))
+(newline)
+(display (new-mul-interval (make-interval -3 5) (make-interval 4 6)))
+
+(newline)
+
+(display (mul-interval (make-interval 4 6) (make-interval -3 5)))
+(newline)
+(display (new-mul-interval (make-interval 4 6) (make-interval -3 5)))
