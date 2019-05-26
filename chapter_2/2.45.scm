ea7daed5db36a390fcafc830aadda05451b059cb
ceoro9 <ceoro9@protonmail.com>
2019-05-26 19:33:04 +0300

    Racket support

--- chapter_2/2.45.scm
+++ chapter_2/2.45.scm
@@ -1,3 +1,5 @@
+(require (planet "sicp.ss" ("soegaard" "sicp.plt" 2 1)))
+
 (define (split main-proc split-proc)
     (lambda (painter n) 
         (if (= n 1)
@@ -5,7 +7,11 @@
             (let ((smaller ((split main-proc split-proc) painter (- n 1))))
                 (main-proc painter (split-proc smaller smaller))))))
 
-
 (define right-split (split beside below))
 
 (define up-split (split below beside))
+
+(paint (right-split einstein 2))
+
+(paint (up-split einstein 2))
+
