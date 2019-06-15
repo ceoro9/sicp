568d3abd1f739340e132b2d87914877a7fe36714
ceoro9 <ceoro9@protonmail.com>
2019-06-15 17:47:03 +0300

    Correct way to handle exponentiation

--- chapter_2/2.56.scm
+++ chapter_2/2.56.scm
@@ -1,4 +1,3 @@
-; TODO;
 (define (deriv exp var)
     (cond ((number? exp) 0)
           ((variable? exp) (if (same-variable? exp var) 1 0))
@@ -27,8 +26,6 @@
     (cond ((=number? a1 0) a2)
           ((=number? a2 0) a1)
           ((and (number? a1) (number? a2)) (+ a1 a2))
-          ((and (number? a1) (sum? a2)) (make-sum (+ a1 (addend a2)) (augend a2)))
-          ((and (number? a2) (sum? a1)) (make-sum (+ a2 (addend a1)) (augend a1)))
           (else (list '+ a1 a2))))
 
 (define (make-product m1 m2)
@@ -41,19 +38,12 @@
 (define (sum? x) (and (pair? x) (eq? (car x) '+)))
 
 (define (addend s) (cadr s))
-(define (augend s)
-    (if (not (pair? (caddr s)))
-        (caddr s)
-        (append (list ('+ 0) (cdr (cdr s))))))
+(define (augend s) (caddr s))
 
 (define (product? x) (and (pair? x) (eq? (car x) '*)))
 
 (define (multiplier p) (cadr p))
-(define (multiplicand p)
-    (if (not (pair? (caddr p)))
-        (caddr p)
-        (append (list ('* 1) (cdr (cdr p))))))
-
+(define (multiplicand p) (caddr p))
 
 (define (pow a n)
     (cond ((= n 0) 1)
@@ -70,5 +60,5 @@
 
 (define (exponentation? x) (and (pair? x) (eq? (car x) '^)))
 
-(display (deriv '(+ x 3 x) 'x))
+(display (deriv '(+ (^ x 2) (^ x 5)) 'x))
 
