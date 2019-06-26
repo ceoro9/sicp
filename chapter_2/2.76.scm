132bf0edb066c017e91c5c4bdfb633d8197ac474
ceoro9 <ceoro9@protonmail.com>
2019-06-26 12:10:14 +0300

    2.75, 2.76: Message passing

---
+++ chapter_2/2.76.scm
@@ -0,0 +1,24 @@
+; General description:
+
+; Explicit dispath:
+; adding new type - pain, need to add conditions to all genetic methods
+; adding new operation - easy, just one condition
+; Main advatange: easy and straight-foward to code
+
+; Message-passing
+; addding new type - easy breezy
+; adding new operation - pain, need to change all the old code
+; Main advantage: easy to maintain
+
+; Data-directed:
+; adding new type - not such easy as message-passing
+; adding new operation - easy
+; Main advantage: scalability
+
+
+; Which organization would be most appropriate for a system in which new types must often be added?
+; answer: Data-directed
+
+; Which would be most appropriate for a system in which new operations must often be added?
+; answer: Message-passing
+
